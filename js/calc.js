4d4092dd88f1d50427eb349f2ca86e84773a65b4
Okafor John Onyinyechukwu <jokafor809@gmail.com>
2025-08-13 06:22:14 -0700

    Update calc.js

--- js/calc.js
+++ js/calc.js
@@ -1,1 +1,46 @@
+// const monthname = document.getElementById('monthname');
+// const dayname = document.getElementById('dayname');
+// const daynumber = document.getElementById('daynumber');
+// const year = document.getElementById('year');
+
+// const date = new Date();
+// monthname.innerText = date.toLocaleString("en", {
+//   month: "long"
+// });
+// dayname.innerText = date.toLocaleString("en", {
+//   weekday: "long"
+// });
+
+// daynumber.innerText = date.getDate();
+// year.innerText = date.getFullYear();
+
+
+// //Events
+// const header = document.getElementById("header");
+
+// header.addEventListener('mouseover', ()=>{
+//   header.style.background = 'blue'
+//   header.innerHTML = 'hello world'
+//   header.style.color = 'white'
+// })
+
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+  display.value +=input
+}
+
+function clearDisplay (){
+  display.value = ""
+}
+function calculate(){
+  try{
+    display.value = eval(display.value)
+  }
+  catch(error){
+    display.value = "Error"
+  }
+}
+  display.value = eval()
 
